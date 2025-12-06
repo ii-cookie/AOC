dfe8e7a14ba7eaa93eb0f991f2ac81087e73d960
ii-cookie <isaacnleong@gmail.com>
2025-12-07 01:20:26 +0800

    yo i first tried the second part, cool

---
+++ 2025/day4/part1.py
@@ -0,0 +1,66 @@
+
+def isPaperRoll(char):
+    if char == "@":
+        return True
+    else: 
+        return False
+    
+def checkSurround(row, col, map):
+    sum = 0
+    if isPaperRoll(map[row + 1][col + 1]):
+        sum += 1
+    if isPaperRoll(map[row + 1][col]):
+        sum += 1
+    if isPaperRoll(map[row + 1][col - 1]):
+        sum += 1
+
+    if isPaperRoll(map[row][col + 1]):
+        sum += 1
+    if isPaperRoll(map[row][col - 1]):
+        sum += 1
+
+    if isPaperRoll(map[row - 1][col + 1]):
+        sum += 1
+    if isPaperRoll(map[row - 1][col]):
+        sum += 1
+    if isPaperRoll(map[row - 1][col - 1]):
+        sum += 1
+    return sum
+
+def canAccess(row, col, map):
+    if isPaperRoll(map[row][col]):
+        if checkSurround(row, col, map) < 4:
+            # print(map[row + 1][col + 1] + map[row + 1][col] + map[row + 1][col - 1] + "\n" + map[row][col + 1] + map[row][col] + map[row][col - 1] + "\n" + map[row - 1][col + 1] + map[row - 1][col] + map[row - 1][col - 1])
+            # print("==========")
+            return True
+    return False
+
+def countAccess(map):
+    access_list = []
+    count = 0
+    for row in range(len(map)):
+        for col in range(len(map[row])):
+            if canAccess(row,col,map):
+                # print(count)
+                access_list.append([row, col])
+                count += 1
+                
+    return count
+grid_map = []
+
+with open("input.txt", "r") as file:
+    width = -1
+    for line in file:
+        width = len(line)
+        grid_row = []
+        for i in range(len(line)):
+            grid_row.append(line[i])
+        grid_map.append(grid_row)
+    
+    last_row = []
+    for i in range(width):
+        last_row.append(".")
+    grid_map.append(last_row)
+
+
+print(countAccess(grid_map))
